17042a354e4e02ca7b1c4be857954b980e9a28a0
encokarthur298 <endev1569@gmail.com>
2025-05-27 21:24:31 +0100

    changed

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'InitFlow' }, { name: 'description', content: 'Build Full-Stack Apps instantly with nocode' }];
 };
 
 export const loader = () => json({});
