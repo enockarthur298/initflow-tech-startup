ec6e03c05b747738538749e36b1647bbcb0dd076
encokarthur298 <endev1569@gmail.com>
2025-05-30 18:28:17 +0100

    made update

--- app/lib/hooks/index.ts
+++ app/lib/hooks/index.ts
@@ -3,4 +3,5 @@ export * from './usePromptEnhancer';
 export * from './useShortcuts';
 export * from './useSnapScroll';
 export * from './useEditChatDescription';
+export * from './StickToBottom';
 export { default } from './useViewport';
