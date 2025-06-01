d71f818ff07e7cd525c84cca79befdea3649949f
encokarthur298 <endev1569@gmail.com>
2025-06-01 08:34:12 +0100

    made update

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -272,9 +272,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                InitFlow
-                </h1>
+                
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
                   Describe your app idea, and we'll guide you through building it step by step
                 </p>
